6381e79bd476a8a6aaa4ccdb63384b4dace5aa2c
Ashwini Kumar <ashwini7security@gmail.com>
2012-11-20 16:07:01 +0530

    Added checkbox's for user to select product

--- items.php
+++ items.php
@@ -6,34 +6,47 @@
  *@status: complete
  */
 
-# including database file 
-#
-include "db_info.php" ; 
 
+?>
+<!DOCTYPE html>
+<html>
+<meta charset='utf-8' lang="en">
+<title>Shopcart|Purchase </title>
+<head>
+<?php
+# including database file
+include "db_info.php" ; 
+?>
+</head>
+
+<body>
+
+<form action='confirms.php' method='post'>
+
+<?php
 # fetching data from table
-$fetching_data = mysql_query("select * from tablename");
+$fetching_data = mysql_query("select * from products");
 
 
 	echo "<h2> Products list </h2>";
 	echo "<table border = \"2\">";
-	echo "<tr><th>Product ID</th><th>Title </th><th width=\"800\"> Description </th><th> Price </th></tr>";
+	echo "<tr><th>Select here</th><th>Product ID </th><th>Title </th><th width=\"800\"> Description </th><th> Price </th></tr>";
 
 
-	while ($products = mysql_fetch_array($fetching_data))
+	while ($products = mysql_fetch_assoc($fetching_data))
 	{
-		echo "<tr><td>" . $products['product_id'] . "</td><td>" . $products['title'] . "</td><td>" . $products['description'] . "</td><td>" . $products['price'] . "</td></tr>" ; 
+		echo "<tr><td>" . 
+		"	<input name=\"products[]\" type=\"checkbox\" value='" . $products['title'] . "'>" . "</td><td>" . $products['product_id'] . "</td><td>" . $products['title'] . "</td><td>" . $products['description'] . "</td><td>" .  "$". $products['price'] . "</td></tr>" ; 
 	}
-
 	echo "</table>";
+	echo "<input type=\"submit\" name=\"submit\" value=\"checkout\">";
+	
 
 
 
 #closing database connection 
-mysql_close($connect);
-
-
-
-
-
-
+#mysql_close($connect);
 ?>
+</form>
+</body>
+</html>
